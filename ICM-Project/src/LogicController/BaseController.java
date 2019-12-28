924d181d7d1de61644ac0e2e4c5c8bdae5fdd1b4
dimaxer <45128443+dimaxer@users.noreply.github.com>
2019-12-28 11:57:06 +0200

    Add files via upload

--- ICM-Project/src/LogicController/BaseController.java
+++ ICM-Project/src/LogicController/BaseController.java
@@ -3,28 +3,35 @@ package LogicController;
 import java.io.IOException;
 
 import Utilities.MessageObject;
-
+import Utilities.ScreenManager;
 import client.Client;
 
 public class BaseController {
 	public void switchScene(String fxml_name) {
-		Client.getInstance().switchScene(fxml_name);
+		try {
+			ScreenManager.getInstance().switchScene(fxml_name);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	/**
-	 * This function returns true if a certain scene already exists, and false otherwise.
-	 * It should be utilized regularly before switching scenes in order to prevent reinitializing FX's and LC's over and over.
-	 * Regular utilization will lead to extreme acceleration of scene switching.
+	 * This function returns true if a certain scene already exists, and false
+	 * otherwise. It should be utilized regularly before switching scenes in order
+	 * to prevent reinitializing FX's and LC's over and over. Regular utilization
+	 * will lead to extreme acceleration of scene switching.
+	 * 
 	 * @author Raz Malka
 	 * @param fxml_name
 	 * @return Boolean indicating whether the scene already exists.
 	 */
 	public Boolean sceneExists(String fxml_name) {
-		return Client.getInstance().sceneExists(fxml_name);
+		return ScreenManager.getInstance().sceneExists(fxml_name);
 	}
 
 	public Object getCurrentFX() {
-		return Client.getInstance().getCurrentFX();
+		return ScreenManager.getInstance().getCurrentFX();
 	}
 
 	/**
