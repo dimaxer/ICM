ff95e82455a87784f9ffc5328fe1e7f66ed8d184
dimaxer <45128443+dimaxer@users.noreply.github.com>
2019-12-28 15:35:11 +0200

    Add files via upload

--- ICM-Project/src/Utilities/RequestType.java
+++ ICM-Project/src/Utilities/RequestType.java
@@ -2,10 +2,5 @@ package Utilities;
 
 //getLinkedRequests is for get all the requests that involve the user ID;
 public enum RequestType {
-	Login,
-	View_Req_Details,
-	Submit_Cng,
-	change_Status,
-	viewUserRequestTable,
-	getReqquestInfo
+	Login, View_Req_Details, Submit_Cng, change_Status, viewUserRequestTable, getReqquestInfo, NewChangeRequest,
 }
