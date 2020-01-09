c9d0cffabfab8d14ca32b2da887f47401ad3ea79
dimaxer <45128443+dimaxer@users.noreply.github.com>
2020-01-09 16:02:31 +0200

    Add files via upload

--- ICM-Project/src/Utilities/RequestType.java
+++ ICM-Project/src/Utilities/RequestType.java
@@ -3,5 +3,7 @@ package Utilities;
 //getLinkedRequests is for get all the requests that involve the user ID;
 public enum RequestType {
 	Login, View_Req_Details, Submit_Cng, change_Status, NewChangeRequest, refreshViewUserRequestTable, AttachFile,
-	viewISDRequestTable, viewRequestTable, ViewAttachedFiles,DownloadAttachedFiles,ApprovedEvaluator, AllUserDetails, InformationSystem_Details, PermanentRoles_Details, UpdateEvaluator, UpdatePermanentRoles
+	viewISDRequestTable, viewRequestTable, ViewAttachedFiles, DownloadAttachedFiles, ApprovedEvaluator, AllUserDetails,
+	InformationSystem_Details, PermanentRoles_Details, UpdateEvaluator, UpdatePermanentRoles,
+	ErrorThisUserIsAllreadyLoggedIn, LogOut
 }
