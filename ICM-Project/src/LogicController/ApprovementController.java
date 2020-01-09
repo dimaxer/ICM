2082f9f12d48576dbeb5c92c59a76b13ec754a12
Noam Keren <57570767+nkrn1111@users.noreply.github.com>
2020-01-09 18:30:35 +0200

    Update ApprovementController.java

--- ICM-Project/src/LogicController/ApprovementController.java
+++ ICM-Project/src/LogicController/ApprovementController.java
@@ -7,22 +7,21 @@ import Utilities.RequestType;
 
 public class ApprovementController extends BaseController {
 
-/**
- * send to the server the evaluator 
- * that was approved by the supervisor
- * @param requestID
- * @param evaluatorID
- */
+	/**
+	 * send to the server the evaluator that was approved by the supervisor
+	 * 
+	 * @param requestID
+	 * @param evaluatorID
+	 */
 	public void approvedEvaluator(String requestID, String evaluatorID) {
-		
+
 		ArrayList<Object> list = new ArrayList<Object>();
-		
+
 		list.add(requestID);
 		list.add(evaluatorID);
-		
-		MessageObject evaluatorMessage =new MessageObject(RequestType.ApprovedEvaluator, list);
+
+		MessageObject evaluatorMessage = new MessageObject(RequestType.ApprovedEvaluator, list);
 		sendMessage(evaluatorMessage);
-		
-		
+
 	}
 }
